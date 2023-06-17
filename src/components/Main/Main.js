163db4e85ce0e4acd0f15183dc55a3e9238ced5b
Maxim Grassi <grassimacsim@yandex.ru>
2023-06-17 13:02:49 +0300

    feat: добавить главную страницу

---
+++ src/components/Main/Main.js
@@ -0,0 +1,16 @@
+import "./Main.scss";
+import Promo from "./Promo/Promo";
+function Main() {
+  return (
+    <div className="main">
+      <Promo></Promo>
+      {/*      <NavTab></NavTab>
+      <AboutProject></AboutProject>
+      <Techs></Techs>
+      <AboutMe></AboutMe>
+      <Portfolio></Portfolio>*/}
+    </div>
+  );
+}
+
+export default Main;
