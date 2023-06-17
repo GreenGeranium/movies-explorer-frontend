8bf7b10a21cca4a975d2359bb5bf6d41a508accd
Maxim Grassi <grassimacsim@yandex.ru>
2023-06-17 14:58:24 +0300

    feat: сделать блок со студентом

---
+++ src/components/Main/AboutMe/AboutMe.js
@@ -0,0 +1,12 @@
+import "./AboutMe.scss";
+
+function AboutMe() {
+  return (
+    <section className="aboutme">
+      <h2 className="aboutme__title">Студент</h2>
+      <div className="aboutme__content"></div>
+    </section>
+  );
+}
+
+export default AboutMe;
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -3,6 +3,7 @@ import Promo from "./Promo/Promo";
 import NavTab from "./NavTab/NavTab";
 import AboutProject from "./AboutProject/AboutProject";
 import Techs from "./Techs/Techs";
+import AboutMe from "./AboutMe/AboutMe";
 function Main() {
   return (
     <main className="main">
@@ -10,10 +11,11 @@ function Main() {
       <NavTab></NavTab>
       <AboutProject></AboutProject>
       <Techs></Techs>
+      <AboutMe></AboutMe>
       {/*
 
 
-      <AboutMe></AboutMe>
+
       <Portfolio></Portfolio>*/}
     </main>
   );
