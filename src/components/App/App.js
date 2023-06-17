2e0b5b2f6d9f9193238adcc2eab1984fbe42b039
Maxim Grassi <grassimacsim@yandex.ru>
2023-06-18 01:09:04 +0300

    feat: начать header зависимый от роутов

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,12 +2,20 @@ import "./App.css";
 import Header from "../Header/Header";
 import Main from "../Main/Main";
 import Footer from "../Footer/Footer";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="page">
       <Header></Header>
-      <Main></Main>
+      <Routes>
+        <Route path="/" element={<Main />}></Route>
+        <Route path="/movies"></Route>
+        <Route path="/saved-movies"></Route>
+        <Route path="/profile"></Route>
+        <Route path="/signin"></Route>
+        <Route path="/signup"></Route>
+      </Routes>
       <Footer></Footer>
     </div>
   );
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,16 +1,52 @@
 import "./Header.scss";
 import logo from "../../images/logomain.svg";
+import { Link, NavLink, useLocation } from "react-router-dom";
 
 function Header() {
-  return (
-    <header className="header">
-      <img src={logo} alt="Логотип Учебного проекта" />
-      <div className="buttons">
-        <a className="buttons__register">Регистрация</a>
-        <button className="buttons__login">Войти</button>
-      </div>
-    </header>
-  );
+  const { pathname } = useLocation();
+  console.log(pathname);
+
+  if (pathname === "/") {
+    return (
+      <header className="header">
+        <Link to="/">
+          <img src={logo} alt="Логотип Учебного проекта" />
+        </Link>
+        <div className="buttons">
+          <Link to="/signup" className="buttons__register">
+            Регистрация
+          </Link>
+          <Link to="/signin">
+            <button className="buttons__login">Войти</button>
+          </Link>
+        </div>
+      </header>
+    );
+  } else if (
+    pathname === "/movies" ||
+    pathname === "/saved-movies" ||
+    pathname === "/profile"
+  ) {
+    return (
+      <header className="header header_type_movies">
+        <Link to="/">
+          <img src={logo} alt="Логотип Учебного проекта" />
+        </Link>
+        <div className="buttons buttons_type_movies">
+          <NavLink to="/movies" className="buttons__movie">
+            Фильмы
+          </NavLink>
+          <NavLink to="/saved-movies" className="buttons__movie">
+            Сохраненные фильмы
+          </NavLink>
+        </div>
+
+        <Link to="/profile">
+          <button> </button>
+        </Link>
+      </header>
+    );
+  }
 }
 
 export default Header;
