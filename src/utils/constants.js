ff7890cf9df9becce5e102e1a15bd39361d620e4
Maxim Grassi <grassimacsim@yandex.ru>
2023-07-01 00:05:13 +0300

    style: добавить README.md

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,2 @@
-export const MAIN_API = "http://api.geranius.nomoredomains.rocks";
+export const MAIN_API = "https://api.geranius.nomoredomains.rocks";
 export const MOVIES_API = "https://api.nomoreparties.co/beatfilm-movies";
